53c2f36ed97013d96f7a4d58fca416bffddaee09
Thay <pattasiwa@gmail.com>
2021-05-30 08:42:58 -0400

    can delete group with button

--- components/ui/GroupCard.js
+++ components/ui/GroupCard.js
@@ -3,15 +3,6 @@ import classes from './GroupCard.module.css'
 import Link from 'next/link'
 import EditGroupForm from '../input/EditGroupForm';
 
-async function deleteGroup(id) {
-
-    const response = await fetch('/api/group/' + id, { method: 'DELETE' })
-    const data = await response.json();
-    if (!response.ok) {
-        throw new Error(data.message || 'Something went wrong!');
-    }
-    return data;
-}
 
 export default function GroupCard(props) {
     const [editFormState, setEditFormState] = useState(false);
@@ -20,13 +11,28 @@ export default function GroupCard(props) {
         setEditFormState(!editFormState)
     }
 
+    const groupId = props.id
+
+    async function deleteGroup() {
+
+        const response = await fetch('/api/group/' + groupId, { method: 'DELETE' })
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error(data.message || 'Something went wrong!');
+        }
+        return data;
+    }
+
+
     return (
         <Fragment>
             <div className={classes.card}>
                 <h3>{props.name}</h3>
                 <button>Add Member</button>
+                <p>Member1</p>
+                <p>Member2</p>
                 <Link href={"/group/" + props.id}><button>View Group</button></Link>
-                {props.owner === props.user && <button>Delete Group</button>}
+                {props.owner === props.user && <button onClick={deleteGroup}>Delete Group</button>}
                 {props.owner === props.user && <button onClick={handleEditForm}>Edit Name</button>}
                 {editFormState && <EditGroupForm handleForm={handleEditForm} name={props.name} id={props.id} />}
             </div>
