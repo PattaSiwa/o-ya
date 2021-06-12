80e6085f533e3e3928be215e1d569f5ace5cbd28
Thay <pattasiwa@gmail.com>
2021-06-12 08:10:16 -0400

    added some art to login and sign up - thanks marcus

--- pages/login.js
+++ pages/login.js
@@ -1,33 +1,57 @@
-import LoginForm from '../components/input/LoginForm'
-import classes from '../styles/pages-styles/login.module.css'
-import { getSession } from 'next-auth/client'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import LoginForm from "../components/input/LoginForm";
+import classes from "../styles/pages-styles/login.module.css";
+import { getSession } from "next-auth/client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 export default function LoginPage() {
-    const [isLoading, setIsLoading] = useState(true)
-    const router = useRouter();
-    useEffect(() => {
-        getSession().then(session => {
-            if (session) {
-                router.replace('/')
-            } else {
-                setIsLoading(false)
-            }
-        })
-    }, [router])
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
 
-    if (isLoading) {
-        return <div className='loadingPage'>
-
-            <h2 className='loading'>Loading...</h2>
-
-        </div>
-    }
-
+  if (isLoading) {
     return (
-        <div className={classes.login}>
-            <LoginForm />
-        </div>
-    )
+      <div className="loadingPage">
+        <h2 className="loading">Loading...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.login}>
+      <LoginForm />
+      <motion.img
+        className={classes.loginArt}
+        src="/art/login.svg"
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            transition: {
+              duration: 1,
+            },
+            opacity: 0,
+            translateX: 300,
+          },
+          visible: {
+            opacity: 0.9,
+            translateX: 0,
+            transition: {
+              delay: 0.5,
+              duration: 1,
+            },
+          },
+        }}
+      ></motion.img>
+    </div>
+  );
 }
--- pages/signup.js
+++ pages/signup.js
@@ -1,33 +1,57 @@
-import classes from '../styles/pages-styles/signup.module.css'
-import SignUpForm from '../components/input/SignupForm'
-import { getSession } from 'next-auth/client'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-
+import classes from "../styles/pages-styles/signup.module.css";
+import SignUpForm from "../components/input/SignupForm";
+import { getSession } from "next-auth/client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 export default function SignupPage() {
-    const [isLoading, setIsLoading] = useState(true)
-    const router = useRouter();
-    useEffect(() => {
-        getSession().then(session => {
-            if (session) {
-                router.replace('/')
-            } else {
-                setIsLoading(false)
-            }
-        })
-    }, [router])
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
 
-    if (isLoading) {
-        return <div className='loadingPage'>
-            <p className='loading'>Loading...</p>
-
-        </div>
-    }
-
+  if (isLoading) {
     return (
-        <div className={classes.signup}>
-            <SignUpForm />
-        </div>
-    )
+      <div className="loadingPage">
+        <p className="loading">Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.signup}>
+      <motion.img
+        className={classes.signupArt}
+        src="/art/signup.svg"
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            transition: {
+              duration: 1,
+            },
+            opacity: 0,
+            translateX: -300,
+          },
+          visible: {
+            opacity: 0.9,
+            translateX: 0,
+            transition: {
+              delay: 0.5,
+              duration: 1,
+            },
+          },
+        }}
+      ></motion.img>
+      <SignUpForm />
+    </div>
+  );
 }
